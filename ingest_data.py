e8f0c7db0291a8af0978f5310cfeb249189259a6
rcarhart <rcarhart2@gmail.com>
2025-01-26 11:51:30 -0500

    finished

--- ingest_data.py
+++ ingest_data.py
@@ -24,33 +24,14 @@ try:
         df = pd.read_csv("green_tripdata_2019-10.csv")
         df.to_sql("green_tripdata_2019_01", engine, if_exists="replace", index=False)
     else:
-        print("Table 'green_tripdata_2019_01' already exists. Checking for data.")
-        with engine.connect() as connection:
-            result = connection.execute("SELECT COUNT(*) FROM green_tripdata_2019_01")
-            row_count = result.scalar()
-        if row_count == 0:
-            print("Table 'green_tripdata_2019_01' is empty. Ingesting data.")
-            df = pd.read_csv("green_tripdata_2019-10.csv")
-            df.to_sql("green_tripdata_2019_01", engine, if_exists="append", index=False)
-        else:
             print("Table 'green_tripdata_2019_01' already has data. Doing nothing.")
 
     # Check and process 'taxi_zone_lookup' table
     if not inspector.has_table("taxi_zone_lookup"):
         print("Table 'taxi_zone_lookup' does not exist. Creating table and ingesting data.")
         df_taxi_zone_lookup = pd.read_csv("taxi_zone_lookup.csv")
-        taxi_zone_lookup_schema = pd.io.sql.get_schema(df_taxi_zone_lookup, name='taxi_zone_lookup', con=engine)
         df_taxi_zone_lookup.to_sql("taxi_zone_lookup", engine, if_exists="replace", index=False)
     else:
-        print("Table 'taxi_zone_lookup' already exists. Checking for data.")
-        with engine.connect() as connection:
-            result = connection.execute("SELECT COUNT(*) FROM taxi_zone_lookup")
-            row_count = result.scalar()
-        if row_count == 0:
-            print("Table 'taxi_zone_lookup' is empty. Ingesting data.")
-            df_taxi_zone_lookup = pd.read_csv("taxi_zone_lookup.csv")
-            df_taxi_zone_lookup.to_sql("taxi_zone_lookup", engine, if_exists="append", index=False)
-        else:
             print("Table 'taxi_zone_lookup' already has data. Doing nothing.")
 
 except Exception as e:
